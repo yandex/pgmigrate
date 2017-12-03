04b098b9c980a8a742be91545a0c2210340d85d5
secwall <secwall@secwall.me>
2017-12-03 17:19:48 +0300

    Set pg major version only in Dockerfile

--- run_test.sh
+++ run_test.sh
@@ -5,6 +5,6 @@ set -e
 chown -R postgres:postgres /dist
 mkdir -p /var/log/pogsgresql
 chown postgres:postgres /var/log/pogsgresql
-sudo -u postgres /usr/lib/postgresql/10/bin/pg_ctl -D /etc/postgresql/10/main -l /var/log/pogsgresql/postgresql-10-main.log start
+sudo -u postgres /usr/lib/postgresql/${PG_MAJOR}/bin/pg_ctl -D /etc/postgresql/10/main -l /var/log/pogsgresql/postgresql-${PG_MAJOR}-main.log start
 cd /dist
 sudo -u postgres -i tox -c /dist/tox.ini
