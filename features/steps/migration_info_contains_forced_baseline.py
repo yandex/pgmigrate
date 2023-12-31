e74474056a2b492114e559150384607dbe52d28a
secwall <secwall@secwall.me>
2023-12-31 20:21:58 +0300

    Allow reordering setting schema version and afterEach callback

--- features/steps/migration_info_contains_forced_baseline.py
+++ features/steps/migration_info_contains_forced_baseline.py
@@ -9,5 +9,5 @@ from pgmigrate import _get_info
 def step_impl(context, baseline, schema='public'):
     cur = context.conn.cursor()
     info = _get_info(context.migr_dir, 0, 1, schema, cur)
-    assert list(info.values())[0]['version'] == int(baseline)
-    assert list(info.values())[0]['description'] == 'Forced baseline'
+    assert list(info.values())[0].meta['version'] == int(baseline)
+    assert list(info.values())[0].meta['description'] == 'Forced baseline'
--- features/steps/migration_info_contains_single_migration.py
+++ features/steps/migration_info_contains_single_migration.py
@@ -7,5 +7,5 @@ from pgmigrate import _get_info
 def step_impl(context, schema='public'):
     cur = context.conn.cursor()
     info = _get_info(context.migr_dir, 0, 1, schema, cur)
-    assert list(info.values())[0]['version'] == 1
-    assert list(info.values())[0]['description'] == 'Single migration'
+    assert list(info.values())[0].meta['version'] == 1
+    assert list(info.values())[0].meta['description'] == 'Single migration'
